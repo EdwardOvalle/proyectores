48a247bc559651da1d9d6ef5390ad480170cf86c
EdwardOvalle <23470279@campeche.tecnm.mx>
2026-04-15 08:36:13 -0600

    Se modifico el nombre del archivo README.md

---
+++ WebApp/Controllers/ProyectoresController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.Controllers
+{
+    public class ProyectoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(
+                "<h1>Proyectores</h1>" +
+                "<p>Aqu&iacute se mostrar&aacute; la lista de proyectores registrados</p>",
+                "text/html"
+                );
+        }
+
+        public IActionResult Details(int id)
+        {
+            string html = "<h1>Nuevo proyector</h1>";
+            html += $"<p>Aqu&iacute se mostrar&aacute la informaci&oacuten del proyector {id}</p>";
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            string html = "<h1>Dar de baja</h1>";
+            html += $"<p>Aqu&iacute se dar&aacute de baja el proyector {id}</p>";
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            string html = "<h1>Editar proyector</h1>";
+            html += $"<p>Aqu&iacute se modificar&aacute la informaci&oacuten del proyector {id}</p>";
+            return Content(html, "text/html");
+        }
+    }
+}
